f654dfddaab4c0834221e0e64931740f33ddd244
Hannah Sewell <hse01@sbgml04155.sbg.skybettingandgaming.com>
2018-09-14 14:18:37 +0100

    make node work

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var express = require('express');
+var express = require ('express');
 var fs = require('fs');
-var path = require('path');
+var path = require ('path')
 var app = express();
-var routes = require('./routes/routes');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-routes(app)
-
-
+app.get("/example", function(req, res){
+    res.render('example', {
+        heading: 'LeaderBoard',
+        msg:'The Current Winners are',
+        myArray:['red', 'yellow', 'green', 'blue'],
+        Leaderboard: [{
+            "name": "fred",
+            "score": 345
+        }, {
+            "name": "Laura",
+            "score": 365
+        },
+        {
+            "name": "Bob",
+            "score": 32
+        },
+        {
+            "name": "Janet",
+            "score": 345
+        }
+    ]
+    })
+})
 
 app.listen(3000)
