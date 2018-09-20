b30f3be4493718ab4f65e8f4558c926d9a36ba21
Stephen Roberts <sro01@sbgml01231.sbg.skybettingandgaming.com>
2018-09-20 13:44:56 +0100

    update predicitons

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var path = require('path');
 var app = express();
 var routes = require('./routes/routes');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(express.static("./super6_test"));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 routes(app)
 
+var MongoClient = require('mongodb').MongoClient
 
+MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, function (err, client) {
+
+app.set('myDb', client.db('gameDataDb'));
+
+})
 
 app.listen(3000)
