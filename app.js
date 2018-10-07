d353672d9aeaf1898ac2ca5ac0fc8858bffc626b
Emilia Lewandowska <emrossa@gmail.com>
2018-10-07 22:18:53 +0100

    Require authentication on certain routes

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ app.use(express.static("./super6-react/build"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        users.lookup(username, password, function (err, user) {
+            if (err) { return done(err); }
+            done(null, user);
+        });
+    }
+));
+
+passport.deserializeUser(function(id, cb) {
+    users.get(id, cb);
+});
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'super6-app', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 routes(app)
 
 var MongoClient = require('mongodb').MongoClient
@@ -50,33 +76,8 @@ app.set('myDb', client.db('gameDataDb'));
     })
 })*/
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        users.lookup(username, password, function (err, user) {
-            if (err) { return done(err); }
-            done(null, user);
-        });
-    }
-));
-
-passport.deserializeUser(function(id, cb) {
-    users.get(id, cb);
-});
-
-passport.serializeUser(function(user, cb) {
-    cb(null, user.id);
-});
-
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'super6-app', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.post('/api/login', 
+app.post('/api/login',
   passport.authenticate('local'),
   function(req, res) {
     res.send(req.user);
